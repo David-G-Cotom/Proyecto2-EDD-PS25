b038c120ea63940f5c923a3a2abb58d78d3f4b3e
David-G-Cotom <brandoncotom202231460@cunoc.edu.gt>
2025-04-20 22:00:36 -0600

    Agregando la Carpeta del Proyecto creado por NetBeans

---
+++ Proyecto2_EDD_PS25/src/main/java/com/mycompany/proyecto2_edd_ps25/Proyecto2_EDD_PS25.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto2_edd_ps25;
+
+/**
+ *
+ * @author Carlos Cotom
+ */
+public class Proyecto2_EDD_PS25 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
